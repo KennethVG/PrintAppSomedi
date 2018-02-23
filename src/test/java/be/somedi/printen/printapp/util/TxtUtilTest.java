99dc952048509071e81483836d7cc684e13a12ab
Kenneth <kenneth.vangijsel@somedi.be>
2018-02-23 11:12:54 +0100

    Added test

--- src/test/java/be/somedi/printen/printapp/util/TxtUtilTest.java
+++ src/test/java/be/somedi/printen/printapp/util/TxtUtilTest.java
@@ -10,20 +10,35 @@ import java.nio.file.Path;
 public class TxtUtilTest {
 
     @Test
-    public void testIsPathToPrint() throws IOException {
-        ClassPathResource resNotToPrint = new ClassPathResource("vulAan.txt");
+    public void testGeenVerslag() throws IOException {
+        ClassPathResource resNotToPrint= new ClassPathResource("geenVerslag.txt");
+        Path path = resNotToPrint.getFile().toPath();
+        boolean toPrint = TxtUtil.isPathWithLetterNotToPrint(path);
+        Assert.assertTrue(toPrint);
+    }
+
+    @Test
+    public void testLetterToPrint() throws IOException {
+        ClassPathResource resToPrint = new ClassPathResource("print.txt");
+        Path path = resToPrint.getFile().toPath();
+        boolean toPrint = TxtUtil.isPathWithLetterNotToPrint(path);
+        Assert.assertFalse(toPrint);
+    }
+
+    @Test
+    public void testLetterToPatient() throws IOException {
+        ClassPathResource resNotToPrint= new ClassPathResource("patient.txt");
         Path path = resNotToPrint.getFile().toPath();
         boolean toPrint = TxtUtil.isPathWithLetterNotToPrint(path);
         Assert.assertTrue(toPrint);
 
-        resNotToPrint= new ClassPathResource("geenVerslag.txt");
-        path = resNotToPrint.getFile().toPath();
-        toPrint = TxtUtil.isPathWithLetterNotToPrint(path);
-        Assert.assertTrue(toPrint);
+    }
 
-        ClassPathResource resToPrint = new ClassPathResource("print.txt");
-        path = resToPrint.getFile().toPath();
-        toPrint = TxtUtil.isPathWithLetterNotToPrint(path);
-        Assert.assertFalse(toPrint);
+    @Test
+    public void testVulAan() throws IOException {
+        ClassPathResource resNotToPrint = new ClassPathResource("vulAan.txt");
+        Path path = resNotToPrint.getFile().toPath();
+        boolean toPrint = TxtUtil.isPathWithLetterNotToPrint(path);
+        Assert.assertTrue(toPrint);
     }
 }
